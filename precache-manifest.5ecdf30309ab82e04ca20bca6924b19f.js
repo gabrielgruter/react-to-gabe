a4eb486e63f3d55bc6b54b9cc54c282b53425709
Gabriel Gruter <gabriel.gruter@gmail.com>
2020-04-14 16:03:38 +0100

    Updates

---
+++ precache-manifest.5ecdf30309ab82e04ca20bca6924b19f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac97d5b7b9c0df46d8d392fc7472f3a9",
+    "url": "/gabrielgruter.com/index.html"
+  },
+  {
+    "revision": "295ec44ce2ec540d5ed2",
+    "url": "/gabrielgruter.com/static/js/2.57394b85.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gabrielgruter.com/static/js/2.57394b85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d6ffd84ac62d0334f77",
+    "url": "/gabrielgruter.com/static/js/main.73c49cb7.chunk.js"
+  },
+  {
+    "revision": "5dccd9b121dc3c8ff0f2",
+    "url": "/gabrielgruter.com/static/js/runtime-main.2948bb82.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3d83021d4123059af5317d38267854df.js"
+  "/gabrielgruter.com/precache-manifest.5ecdf30309ab82e04ca20bca6924b19f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/gabrielgruter.com/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
