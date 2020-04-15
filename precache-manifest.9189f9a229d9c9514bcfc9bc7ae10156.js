e431daab7cc6c94940c1e6b992fca930e1cc9dcd
Gabriel Gruter <gabriel.gruter@gmail.com>
2020-04-15 11:03:19 +0100

    Updates

---
+++ precache-manifest.9189f9a229d9c9514bcfc9bc7ae10156.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "094e76ada88ca0593a6cceb12431845d",
+    "url": "/react-to-gabe/index.html"
+  },
+  {
+    "revision": "5302b4816eeb0c6f2dc9",
+    "url": "/react-to-gabe/static/js/2.9e42a1b7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-to-gabe/static/js/2.9e42a1b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef7bde1bcea4b105dbe5",
+    "url": "/react-to-gabe/static/js/main.41e569ec.chunk.js"
+  },
+  {
+    "revision": "7bf3bac844c3ad64303a",
+    "url": "/react-to-gabe/static/js/runtime-main.d6cc3267.js"
+  }
+]);
