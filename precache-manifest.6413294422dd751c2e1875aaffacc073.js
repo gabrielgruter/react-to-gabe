fa98536b7d7c9dabd19a5edf06381f42cd3b6176
Gabriel Gruter <gabriel.gruter@gmail.com>
2020-04-16 11:21:07 +0100

    Updates

---
+++ precache-manifest.6413294422dd751c2e1875aaffacc073.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a4310c4ecc04d4103830f072590ba3c",
+    "url": "/www.gabrielgruter.com/index.html"
+  },
+  {
+    "revision": "35ef3378269688b3acb1",
+    "url": "/www.gabrielgruter.com/static/js/2.283009eb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/www.gabrielgruter.com/static/js/2.283009eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "651d4ef0e8f7ed29ee33",
+    "url": "/www.gabrielgruter.com/static/js/main.f6090862.chunk.js"
+  },
+  {
+    "revision": "dc05289dc0956cce5adc",
+    "url": "/www.gabrielgruter.com/static/js/runtime-main.277bace1.js"
+  }
+]);
